abdf8c4546d2307939dfa6404bd34bf7dbceab3e
antoine <antoine@delaunay.org>
2023-12-16 23:52:23 +0100

    correct semantic if of merging files, not just moving one group. Added draft dynamic link colour selection

--- esp.js
+++ esp.js
@@ -8,12 +8,12 @@ function idget(id) {
 
 function tojson(ob) {
   return JSON.stringify(ob, function (key, value) {
-  if(value instanceof Map) {
-    return Array.from(value.entries()); // or with spread: value: [...value]
-  } else {
-    return value;
-  }
-}, 2);
+    if(value instanceof Map) {
+      return Array.from(value.entries()); // or with spread: value: [...value]
+    } else {
+      return value;
+    }
+  }, 2);
 }
 
 function jsonfrom(id) {
@@ -49,7 +49,7 @@ function visiblelog(data) {
 }
 
 function ifdef(o) {
-return typeof o !== 'undefined';
+  return typeof o !== 'undefined';
 }
 function uuidv4() {
   //from http://stackoverflow.com/questions/105034/ddg#2117523
@@ -83,171 +83,202 @@ function setQuery(id, json) {
 //function ESPSystem(config, systems) {
 function ESPSystem() {
 
-    var nextIGID = 0;
-    var nextIFID = 0;
-    var files= new Map();
-    var links = [];
+  var nextIGID = 0;
+  var nextIFID = 0;
+  var files= new Map();
+  var links = [];
 
 
 
-    this.addIG = function(buttonId) {
-        files.set(    
-            nextIFID,{ 
-            ifid:nextIFID,
-            groups: [{IGID:nextIGID}], 
-            excludedGroups: []
-            });
-        refreshLeftIGList();
-        refreshRightIGList();
-        removeInconsistentLinkColoursList();
-        show("groupsShowroom", files);
-        nextIGID ++;
-        nextIFID ++;
-        idget(buttonId).innerHTML= "add IG " + nextIGID;
-    };
+  this.addIG = function(buttonId) {
+    files.set(    
+      nextIFID,{ 
+        ifid:nextIFID,
+        groups: [{IGID:nextIGID}], 
+        excludedGroups: []
+      });
+    refreshLeftIGList();
+    refreshRightIGList();
+    refreshLinkColoursList();
+    show("groupsShowroom", files);
+    nextIGID ++;
+    nextIFID ++;
+    idget(buttonId).innerHTML= "add IG " + nextIGID;
+  };
 
-    var refreshLeftIGList = function() {
-        var select = idget("leftIG");
-       cleanSelect(select); 
-       files.forEach(function(file) {
-           file.groups.forEach(function(freeGroup) {
-               var groupOption = document.createElement("option"); 
-               groupOption.text = freeGroup.IGID;
-               select.add(groupOption);
-           });
-       });
-    };
+  var refreshLeftIGList = function() {
+    var select = idget("leftIG");
+    cleanSelect(select); 
+    files.forEach(function(file) {
+      file.groups.forEach(function(freeGroup) {
+        var groupOption = document.createElement("option"); 
+        groupOption.text = freeGroup.IGID;
+        select.add(groupOption);
+      });
+    });
+  };
 
-    var refreshRightIGList = function() {
-        var currentlyOnLeft = currentValue ("leftIG");
-        var select = idget("rightIG");
-       cleanSelect(select); 
-       files.forEach(function(file) {
-           file.groups.forEach(function(freeGroup) {
-            
-            if (freeGroup.IGID != currentlyOnLeft) {
-           var groupOption = document.createElement("option"); 
-           groupOption.text = freeGroup.IGID;
-           select.add(groupOption);
-           }
-       });
-       });
-    };
+  var refreshRightIGList = function() {
+    var currentlyOnLeft = currentValue ("leftIG");
+    var select = idget("rightIG");
+    cleanSelect(select); 
+    files.forEach(function(file) {
+      file.groups.forEach(function(freeGroup) {
 
-
-    this.getNextIGIDForButton = function() {
-        return nextIGID;
-    };
-
-    this.leftIGChosen= function() {
-        refreshRightIGList();
-    };
-
-    this.rightIGChosen= function() {
-        removeInconsistentLinkColoursList();
-    };
-
-    this.addLink = function() {
-        var colour = currentValue ("linkColour");
-        var currentlyOnLeft = currentValue ("leftIG");
-        var currentlyOnRight = currentValue ("rightIG");
-        links.push ({colour:colour, left:currentlyOnLeft, right:currentlyOnRight}); 
-        //merge-forcesplit files
-        if (colour === 'WL' || colour === 'MRL') {
-        //merge
-        moveInSameFile(currentlyOnLeft, currentlyOnRight);
-        } else if (colour === 'NMRL' || colour === 'GL') {
-        //forcesplit
-        keepInDifferentFiles(currentlyOnLeft, currentlyOnRight);
+        if (freeGroup.IGID != currentlyOnLeft) {
+          var groupOption = document.createElement("option"); 
+          groupOption.text = freeGroup.IGID;
+          select.add(groupOption);
         }
+      });
+    });
+  };
 
-        show("linksShowroom", links);
-        show("groupsShowroom", files);
-    };
 
-    var moveInSameFile = function(leftIG, rightIG) {
-    var leftIf = (fileOfGroup (leftIG));
-    var rightIf = (fileOfGroup (rightIG));
-    if (leftIf === rightIf) {
-        log(leftIG +" and "+rightIG+" are already in the same file "+leftIf+" nothing to do");
+  this.getNextIGIDForButton = function() {
+    return nextIGID;
+  };
+
+  this.leftIGChosen= function() {
+    refreshRightIGList();
+    refreshLinkColoursList();
+  };
+
+  this.rightIGChosen= function() {
+    refreshLinkColoursList();
+  };
+
+  this.addLink = function() {
+    var colour = currentValue ("linkColour");
+    var currentlyOnLeft = currentValue ("leftIG");
+    var currentlyOnRight = currentValue ("rightIG");
+    //making sure the lower group is always on the left to facilitate later matching
+    links.push ({colour:colour, 
+      lower:(currentlyOnLeft<currentlyOnRight?currentlyOnLeft:currentlyOnRight), 
+      higher:(currentlyOnLeft < currentlyOnRight?currentlyOnRight:currentlyOnLeft)}); 
+    //merge-forcesplit files
+    if (colour === 'WL' || colour === 'MRL') {
+      //merge
+      mergeFile(currentlyOnLeft, currentlyOnRight);
+    } else if (colour === 'NMRL' || colour === 'GL') {
+      //forcesplit
+      keepInDifferentFiles(currentlyOnLeft, currentlyOnRight);
+    }
+
+    show("linksShowroom", links);
+    show("groupsShowroom", files);
+    refreshLinkColoursList();
+  };
+
+  var mergeFile = function(leftIG, rightIG) {
+    var leftIfId = (fileOfGroup (leftIG));
+    var rightIfId = (fileOfGroup (rightIG));
+    if (leftIfId === rightIfId) {
+      log(leftIG +" and "+rightIG+" are already in the same file "+leftIfId+" nothing to do");
     } else {
-        //TODO check excluded groups
-        files.get(leftIf).groups.push(takeGroupFromFile(rightIG, rightIf));
-    }
-    
+      //TODO check excluded groups
+      //merging right onto left
+      var leftFile = files.get(leftIfId);
+      files.get(rightIfId).groups.forEach(function(group) {
+        leftFile.groups.push(group);
+      });
 
-    }
-
-    var fileOfGroup= function(group) {
-        var foundIfid;
-
-        files.forEach(function(file, ifid) {
-        if ( !ifdef(foundIfid)) {
-                file.groups.every(function(freeGroup ) {
-                        if (freeGroup.IGID == group) {
-                        foundIfid = ifid;
-                        return false;
-
-                        } else {
-                        return true;}
-                        });
-                        }
-                });
-
-
-        return foundIfid;
+      files.delete(rightIfId);
     }
 
-    var takeGroupFromFile = function(IGID, IFID) {
-    var newGroups = []; 
-    var takenGroup;
-    var file = files.get(IFID);
-    for (var i = 0; i<file.groups.length; i++) {
-    var group = file.groups[i];
-    if (group.IGID == IGID) {
-        takenGroup = group;
-    } else {
-    newGroups.push(group);
 
-    }
-    }
+  }
 
-    if (newGroups.length > 0) {
-        files.get(IFID).groups = newGroups;
-    } else {
-        files.delete(IFID);
-    }
+  var fileOfGroup= function(group) {
+    var foundIfid;
 
-    return takenGroup;
+    files.forEach(function(file, ifid) {
+      if ( !ifdef(foundIfid)) {
+        file.groups.every(function(freeGroup ) {
+          if (freeGroup.IGID == group) {
+            foundIfid = ifid;
+            return false;
+
+          } else {
+            return true;}
+        });
+      }
+    });
 
 
-    }
+    return foundIfid;
+  }
 
-    var keepInDifferentFiles = function(leftIG, rightIG) {
+
+  var keepInDifferentFiles = function(leftIG, rightIG) {
     loge("keepInDifferentFiles not implemented");
+  }
+
+  var currentValue = function(selectId) {
+    var select= idget(selectId);
+    if (select.selectedIndex>=0) {
+    return select.options[select.selectedIndex].value;
     }
+  };
 
-    var currentValue = function(selectId) {
-        var leftIGs= idget(selectId);
-        return leftIGs.options[leftIGs.selectedIndex].value;
-    };
-
-    var removeInconsistentLinkColoursList= function() {
+  var refreshLinkColoursList= function() {
     //we need to know the IF to ensure no consistent issue is created
-    loge("removeInconsistentLinkColoursList not implemented");
-    };
+    var currentlyOnLeft = currentValue ("leftIG");
+    var currentlyOnRight = currentValue ("rightIG");
+    if (ifdef(currentlyOnLeft) && ifdef(currentlyOnRight)) {
+    var lowerIgId = (currentlyOnLeft < currentlyOnRight?currentlyOnLeft:currentlyOnRight);
+    var higherIgId = (currentlyOnLeft < currentlyOnRight?currentlyOnRight:currentlyOnLeft);
 
-    var cleanSelect = function (select) {
-        var i, L = select.options.length - 1;
-        for(i = L; i >= 0; i--) {
-            select.remove(i);
-        }
-    };
+    var select = idget("linkColour");
+    cleanSelect(select); 
 
-    this.getIGs = function() {
-        return groups;
-    };
+    var canHaveWL = true;
+    var canHaveMRL = true;
+    var canHaveNMRL = true;
+    var canHaveGL = true;
+
+
+    links.forEach(function(link) {
+      if (link.colour != 'YL' && link.lower == lowerIgId && link.higher == higherIgId) {
+        //already a non YL link
+        canHaveWL = false;
+        canHaveMRL = false;
+        canHaveNMRL = false;
+        canHaveGL = false;
+      }
+    });
+
+    addColourIfPossible(canHaveWL, 'White', 'WL', select);
+    addColourIfPossible(canHaveMRL, 'Merging red', 'MRL', select);
+    addColourIfPossible(canHaveNMRL, 'Non merging red', 'NMRL', select);
+    addColourIfPossible(canHaveGL, 'Green', 'GL', select);
+
+    //assuming multiple yellows are always possible, no case 10 prevalence implemented
+    addColourIfPossible(true, 'Yellow', 'YL', select);
     }
+
+  };
+
+  var addColourIfPossible = function(possible, text, value, select) {
+    if (possible) {
+    var option = document.createElement("option"); 
+    option.text = text;
+    option.value = value;
+    select.add(option);
+    }
+  };
+
+  var cleanSelect = function (select) {
+    var i, L = select.options.length - 1;
+    for(i = L; i >= 0; i--) {
+      select.remove(i);
+    }
+  };
+
+  this.getIGs = function() {
+    return groups;
+  };
+}
 
 
 var esp = new ESPSystem();
@@ -263,9 +294,9 @@ doOnClick("addLinkButton", function () {
 });
 
 idget("leftIG").onchange = function() {
-    esp.leftIGChosen();
+  esp.leftIGChosen();
 };
 
 idget("rightIG").onchange = function() {
-    esp.rightIGChosen();
+  esp.rightIGChosen();
 };
